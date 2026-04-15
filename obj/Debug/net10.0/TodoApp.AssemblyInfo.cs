c5738e2caaae39403035a1c6f17f932bdecc574c
pak20116 <30700291+pak20116@users.noreply.github.com>
2026-04-15 02:15:22 -0700

    pak20116 first change commit

--- obj/Debug/net10.0/TodoApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a62bdd22ee433834f82b41525fccc6f67369e2f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
